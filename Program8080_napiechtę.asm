435a715946e6a4d8489921345f64dfa7a15ad476
Lisiozmur <115721790+Lisiozmur@users.noreply.github.com>
2022-11-01 16:57:43 +0100

    Add files via upload

---
+++ Program8080_napiechtę.asm
@@ -0,0 +1,20 @@
+ORG 800H  
+	LXI H,TEKST1  
+	RST 3  
+	LXI H,TEKST2  
+	RST 3  
+	LXI H,TEKST3  
+	RST 3  
+	LXI H,TEKST4  
+	RST 3  
+	LXI H,TEKST5  
+	RST 3  
+	LXI H,TEKST6  
+	RST 3  
+	HLT  
+TEKST1 	 DB '   &&   @'   
+TEKST2 	 DB 10,13,'  &  &  @'   
+TEKST3 	 DB 10,13,' &    & @'   
+TEKST4 	 DB 10,13,' &&&&&& @'   
+TEKST5 	 DB 10,13,'  &&&&  @'   
+TEKST6 	 DB 10,13,'   &&   @'
