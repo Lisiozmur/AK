1410e1eac6b55e8c6919f76710028cb5049ff068
Lisiozmur <115721790+Lisiozmur@users.noreply.github.com>
2022-11-10 12:41:06 -0800

    Create Program8086_napiechtę.asm

---
+++ Program8086_napiechtę.asm
@@ -0,0 +1,9 @@
+ include 'emu8086.inc'
+ ORG 100h
+    PRINTN '   &&   '   
+    PRINTN '  &  &  '   
+    PRINTN ' &    & '   
+    PRINTN ' &&&&&& '   
+    PRINTN '  &&&&  '   
+    PRINTN '   &&   ' 
+ RET 
